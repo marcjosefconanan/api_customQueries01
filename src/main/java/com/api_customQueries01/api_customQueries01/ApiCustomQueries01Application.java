636bdb4260ae3a574f79720be86a4415f5649342
Marc Josef Conanan <coanananmarc@gmail.com>
2024-04-19 18:10:40 +0200

    completato api_customQueries01

---
+++ src/main/java/com/api_customQueries01/api_customQueries01/ApiCustomQueries01Application.java
@@ -0,0 +1,13 @@
+package com.api_customQueries01.api_customQueries01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCustomQueries01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCustomQueries01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/api_customQueries01/api_customQueries01/ApiCustomQueries01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api_customQueries01.api_customQueries01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCustomQueries01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
